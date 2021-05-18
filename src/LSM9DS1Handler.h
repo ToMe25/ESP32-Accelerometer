af93bb8b952abcbd281095648115bb58127070a3
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2021-05-18 21:06:09 +0200

    Add LICENSE headers to source files

--- src/LSM9DS1Handler.h
+++ src/LSM9DS1Handler.h
@@ -1,8 +1,21 @@
 /*
  * LSM9DS1Handler.h
  *
- *  Created on: 27.01.2021
- *      Author: ToMe25
+ * Created on: 27.01.2021
+ *
+ * Copyright 2021 ToMe25
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef SRC_LSM9DS1HANDLER_H_
